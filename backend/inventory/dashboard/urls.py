2eeb9ba970fd7b3b79380fc8baa40e4e4abacc44
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2023-09-15 11:58:37 +0530

    Home page added

--- backend/inventory/dashboard/urls.py
+++ backend/inventory/dashboard/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index , name = 'index'),
+    path('index.html',views.index , name = 'index'),
+    path('', views.home , name = 'home'),
     path('staff.html',views.staff , name = 'staff'),
     path('product.html',views.product , name = 'product'),
     path('order.html',views.order, name = 'order'),
