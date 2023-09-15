969b5d28c34bd50115eb235a33860c1255aa4d9c
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2023-09-15 13:59:32 +0530

    backend for registration completed

--- backend/inventory/accounts/views.py
+++ backend/inventory/accounts/views.py
@@ -1,7 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 # Create your views here.
-def register(request):
+def login(request):
     
     if request.method == 'POST':
-        username = request
+        username = request.POST['Username'],
+        password = request.POST['Password'],
+        
+        user = auth.authenticate(username = username , password = password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request , 'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request , 'login.html')
+
+
+def register(requset):
+    
+    if requset.method == 'POST':
+        username = requset.POST['Username'],
+        password1 = requset.POST['Password1'],
+        password2 = requset.POST['Password2'],
+        email = requset.POST['Email'],
+        
+        if (password1 == password2):
+            if User.objects.filter(username = username).exists():
+                messages.info(request , 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email = email).exists():
+                messages.info(request , 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password = password1 , email = email)
+                user.save()
+                return redirect('login')
+    else:
+        messages.info(request, 'register.html')
+        return render('register')
+    
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+        
+                    
