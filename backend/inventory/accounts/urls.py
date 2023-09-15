f038c78c28dc295809d3568feb086e21de1f880d
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2023-09-15 20:39:07 +0530

    Properly Linked All Pages

--- backend/inventory/accounts/urls.py
+++ backend/inventory/accounts/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('login',views.login , name = 'login'),
     path('register' , views.register , name= 'register'),
     path('logout' , views.logout , name = 'logout'),
-    path('index' , views.index , name = 'index'),
-    path('product' , views.product , name = 'product'),
+    # path('index' , views.index , name = 'index'),
+    # path('product' , views.product , name = 'product'),
 ]
