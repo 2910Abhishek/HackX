63e170a71160302efc566a12ff92d80ebd67a95b
Abhishek Parmar <131667318+2910Abhishek@users.noreply.github.com>
2023-09-15 16:07:12 +0530

    Registration portal linked to main page

--- backend/inventory/accounts/urls.py
+++ backend/inventory/accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('login',views.login , name = 'login'),
     path('register' , views.register , name= 'register'),
     path('logout' , views.logout , name = 'logout'),
+    path('index' , views.index , name = 'index'),
 ]
